9ad797cd49ed09d1704183ca7461aa5270a65453
Muhammad Umar Khan <umar.khan@arbisoft.com>
2018-12-04 18:44:25 +0500

    BookList app

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+import BooksReducer from "./reducer_books";
+import ActiveBook from "./reducer_avtive_book";
+
+const rootReducer = combineReducers({
+  books: BooksReducer,
+  activeBook: ActiveBook
+});
+
+export default rootReducer;
